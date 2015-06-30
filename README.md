2b0b135346fc03f3d9803ada7c5bed9739f66bd2
lakinsm <Steven.Lakin@colostate.edu>
2015-06-30 11:28:11 -0400

    June 30th material

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # hquery
-Windows GUI for batch queries to the HUGO gene database
+A wxPython GUI for Windows to query the HUGO gene database
+
+### Contact Information
+
+This program was written for NIH BTRIS to retrieve information on genes from
+the [HUGO gene database](http://www.genenames.org/) API.
+
+It is maintained by [Steven Lakin](mailto:Steven.Lakin@colostate.edu)
+
+### Getting started
+
+The hquery.py file is provided for testing and debugging purposes.  End users
+will want to use the hquery.pyw to suppress the python console.  To start up
+the program, ensure that you have the necessary dependencies installed.  You 
+will need the following to be installed on your machine:
+
++ [Python 3.x.x](https://www.python.org/downloads/)
++ [Requests Python package](http://docs.python-requests.org/en/latest/user/install/#install)
+
+Double click on hquery.pyw to open the program.  Select "Open File" from the
+"File" menu (File > Open File) and choose a gene batch file from your machine.
+Gene batch files should be utf-8 (standard encoding) text files (.txt) that
+contain one gene symbol per line:
+
+Example:
+
+```
+BRCA1
+MAPK
+ZNF3
+```
+
+
+The gene symbols should be standard (less than 6 characters or otherewise as
+indicated by HGNC).  Improperly formatted gene symbols may return no result.
+Null results are recorded in the RejectedQueryReport file that this program
+outputs.
+
+The location of your gene batch (input) file will be the same directory to which
+this program writes.  The following files will be output after each query:
+
++ GeneQueryResults_[timestamp].txt
++ RejectedQueryReport_[timestamp].txt
+
+The GeneQueryResults file contains the requested gene information in a format
+that can be subsequently fed into the NCI Apellon OntyLog batch uploader. The
+RejectedQueryReport contains a list of all gene symbols that returned no results
+or returned an error code from the API.  If an error code was returned, its
+code will be listed as a standard [REST API status code](http://www.restapitutorial.com/httpstatuscodes.html).
+
+For very large queries (more than 1000 gene symbols), you should first contact
+the [HGNC](http://www.genenames.org/about/contact-details) to request permission for a large query.
+
+This program has no built-in wait time between queries, so running very large
+queries without contacting HGNC could result in a temporary denial of service
+from the server.
+
+For questions or update requests, contact me at the email listed above.
--- hquery.py
+++ hquery.py
@@ -13,6 +13,7 @@
 #-------------------------------------------------------------------------------
 
 # Note: this module requires Python 3
+# This is a wxPython GUI
 
 import os
 import wx
@@ -23,42 +24,58 @@ try:
 except ImportError:
     print('Please install the requests package: pip install requests')
     raise
-    
-def dumpclean(obj, outfile):
+
+## Dictionary parser for requests.json() object, outputs data into
+## batch upload format for the NCI Apellon Ontylog program extension
+def dumpClean(obj, outfile):
     if type(obj) == dict:
         for k, v in obj.items():
             if hasattr(v, '__iter__'):
                 outfile.write(str(k)+"\n")
-                dumpclean(v, outfile)
+                dumpClean(v, outfile)
             else:
-                outfile.write('%s : %s' % (k, v))
+                outfile.write('%s : %s' % (k, v)+"\n")
     elif type(obj) == list:
         for v in obj:
             if hasattr(v, '__iter__'):
-                dumpclean(v, outfile)
+                dumpClean(v, outfile)
             else:
-                outfile.write(str(v)+"\n")
+                outfile.write("\t"+str(v)+"\n")
     else:
-        outfile.write(str(obj)+"\n")
-    
+        outfile.write(+str(obj)+"\n")
+
+## Ensure gene symbol or term is in a valid format
+def validateGene(geneKey):
+    temp = ''
+
+## Use the requests package to query the HUGO gene database for each gene
+## symbol.  This method has built-in gene symbol validation and calls back
+## to the GUI to update the progress bar.  Outfiles are written to the same
+## directory as the input files.
 def hugoQuery(List, Dir):
-    print(Dir)
+    
+    # HUGO API URL, return json object
     prefix = 'http://rest.genenames.org/fetch/symbol/'
     headers = {'Accept': 'application/json'}
     
+    # Set variables for wx.ProgressDialog
     frame.count = 0
     frame.progressMax = len(List)
     frame.keepGoing = True
     frame.skip = False
     
+    # Initialize wx.ProgressDialog
     frame.dialog = wx.ProgressDialog("Query Status", "Query completion:", frame.progressMax, style=wx.PD_APP_MODAL | wx.PD_CAN_ABORT | wx.PD_ELAPSED_TIME)
-                    
+    
+    # Loop over the gene list, check for errors in gene symbol, and query
+    # each gene against the HUGO database.  Return json object and parse it
+    # with dumpclean().  Update the progress bar.
     with open(Dir+"\GeneQueryResults.txt", 'w') as f:
-        with open(Dir+"\QueryReport.txt", 'w') as g:
+        with open(Dir+"\RejectedQueryReport.txt", 'w') as g:
             for gene in List:
                 r = requests.get(prefix+gene, headers=headers)
-                if r.status_code is 200:
-                    dumpclean(obj=r.json()['response']['docs'][0], outfile=f)
+                if r.status_code == 200:
+                    dumpClean(obj=r.json()['response']['docs'][0], outfile=f)
                     f.write("\n\n")
                 else:
                     g.write(str(r.status_code)+": "+gene+"\n")
@@ -66,24 +83,29 @@ def hugoQuery(List, Dir):
                 (frame.keepGoing, frame.skip) = frame.dialog.Update(frame.count, str(frame.count)+" of "+str(frame.progressMax))
     frame.dialog.Destroy()
 
+
+### main GUI app class
 class MainWindow(wx.Frame):
 
+    ## Initialize the main frame
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title, size=(-1,-1))
-        #panel = wx.Panel(self)
-        self.CreateStatusBar()
         
+        # Add status bar and menu
+        self.CreateStatusBar()
         filemenu = wx.Menu()
         
-        menuOpen = filemenu.Append(wx.ID_OPEN, "&Load File", " Load a gene batch file")
+        # File menu options
+        menuOpen = filemenu.Append(wx.ID_OPEN, "&Load File", " Load a gene batch file for query")
         menuAbout = filemenu.Append(wx.ID_ABOUT, "&About", " Information about hquery")
-        menuGit = filemenu.Append(wx.ID_ANY, "&GitHub Repository", " See up-to-date information and source code on GitHub")
+        menuGit = filemenu.Append(wx.ID_ANY, "&Help and Source", " See documentation and source code on GitHub")
         menuExit = filemenu.Append(wx.ID_EXIT, "&Exit", " Terminate the Program")
         
         menuBar = wx.MenuBar()
         menuBar.Append(filemenu, "&File")
         self.SetMenuBar(menuBar)
         
+        # File menu events
         self.Bind(wx.EVT_MENU, self.OnOpen, menuOpen)
         self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
         self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
@@ -91,8 +113,8 @@ class MainWindow(wx.Frame):
         
         self.Show(True)
         
+    ## Method for loading files
     def OnOpen(self, e):
-        """ Open a file """
         self.dirname = ''
         dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", "*.*", wx.OPEN)
         if dlg.ShowModal() == wx.ID_OK:
@@ -112,19 +134,22 @@ class MainWindow(wx.Frame):
                     hugoQuery(geneList, self.dirname)
                 dlg2.Destroy()
         dlg.Destroy()
-    
+        
+    ## Method for link to GitHub repo
     def OnGit(self, e):
         webbrowser.open("https://github.com/lakinsm/hquery")
-        
+    
+    ## Method for program information
     def OnAbout(self, e):
         dlg = wx.MessageDialog(self, "Author: Steven Lakin\nContact: Steven.Lakin@colostate.edu\n\nThis program was developed to batch query gene keys against the HUGO database.\n\nSee https://github.com/lakinsm/hquery for details and current information.", "About hquery", wx.OK)
         dlg.ShowModal()
         dlg.Destroy()
     
+    ## Method for exiting
     def OnExit(self, e):
         self.Close(True)
         
-
+### Main ###
 app = wx.App(False)
 frame = MainWindow(None, 'Batch Query for HUGO Database')
 app.MainLoop()
